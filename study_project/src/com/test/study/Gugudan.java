e1537152d7fcdb1415a92d0be366c01f94803dbd
background <background@background-PC>
2017-11-30 19:43:20 +0900

    commit Gugudan.java

---
+++ study_project/src/com/test/study/Gugudan.java
@@ -0,0 +1,15 @@
+package com.test.study;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		for(int i=2; i <10; i++){
+			System.out.println(i+"단");
+			for(int j=1; j<10; j++){
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+		}
+		
+	}
+
+}
